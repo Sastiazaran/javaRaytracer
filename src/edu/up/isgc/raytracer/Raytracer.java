eef8a71565e515d32e5588e174db19e9819e6f86
Sastiazaran <0226403@up.edu.mx>
2021-05-26 03:59:47 -0500

    Added Refraction

--- src/edu/up/isgc/raytracer/Raytracer.java
+++ src/edu/up/isgc/raytracer/Raytracer.java
@@ -10,6 +10,7 @@ import edu.up.isgc.raytracer.lights.PointLight;
 import edu.up.isgc.raytracer.objects.*;
 import edu.up.isgc.raytracer.tools.OBJReader;
 
+
 import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
@@ -27,26 +28,50 @@ public class Raytracer {
         System.out.println(new Date());
         Scene scene01 = new Scene();
 
-        Material sphere = new Material(.2f, .2f, .1f, 1, 0);
-        Material teaPot = new Material(.2f, 1f, 10f, 1, 0);
-        Material material2 = new Material(.2f, 10f, 20f, 1, 0);
+        Material sphere = new Material(.2f, .2f, .1f, 0, 1);
+        Material teaPot = new Material(.2f, 1f, 10f, .2f, 0);
+        Material plane = new Material(.2f, 10f, 20f, .2f, 0);
         Material ring = new Material(.2f, 0f, 40f, 1, 0);
+        Material Wolf = new Material(.2f, 0f, 0f, 0, 0);
+        Material Candle = new Material(.2f, 0f, 0f, 0, 0);
+        Material Batarang = new Material(.2f, 0f, 0f, 0, 1);
+        Material Joker = new Material(.2f, 10f, 40f, 1, 0);
 
-        scene01.setCamera(new Camera(new Vector3D(0, 0, -8), 160, 160, 400, 400, 5f, 50f, null));
+
+
+        scene01.setCamera(new Camera(new Vector3D(0, 0, -8), 160, 160, 50, 50, -10f, 50f, null));
         /*scene01.addLight(new DirectionalLight(Vector3D.ZERO(), new Vector3D(0.0, 0.0, 1.0), Color.WHITE, 0.8));
         scene01.addLight(new DirectionalLight(Vector3D.ZERO(), new Vector3D(0.0, -0.1, 0.1), Color.WHITE, 0.2));*/
         //scene01.addLight(new DirectionalLight(Vector3D.ZERO(), new Vector3D(-0.2, -0.1, 0.0), Color.WHITE, 0.2, null));
-        scene01.addLight(new PointLight(new Vector3D(0f, 0f, 0f), Color.WHITE, 1.3, null));
-         //scene01.addObject(new Sphere(new Vector3D(0f, 1f, 5f), 0.5f, Color.RED, teaPot));
+        //scene01.addLight(new PointLight(new Vector3D(-3f, 3.5f, 4f), Color.WHITE, 2, null));
+        scene01.addLight(new PointLight(new Vector3D(0f, 3.5f, 8f), Color.GREEN, .76, null));
+        scene01.addLight(new PointLight(new Vector3D(2f, 3.5f, 8f), Color.RED, .2, null));
+        scene01.addLight(new PointLight(new Vector3D(-1f, 3.5f, 8f), new Color(0, 123, 107), .3, null));
+        //scene01.addLight(new PointLight(new Vector3D(1f, 6f, 0f), new Color(255, 75, 75), 1.5, null));
+        //scene01.addLight(new PointLight(new Vector3D(4f, 6f, -1f), new Color(255, 191, 0), 1.5 , null));
+        //scene01.addLight(new PointLight(new Vector3D(0f, 1f, -5f), Color.WHITE, 1.7, null));
+        //scene01.addObject(new Sphere(new Vector3D(1f, -2f, 2f), 0.5f, Color.GRAY, sphere));
         //scene01.addObject(new Sphere(new Vector3D(0.5f, 1f, 4.5f), 0.25f, new Color(200, 255, 0)));
         //scene01.addObject(new Sphere(new Vector3D(0.35f, 1f, 4.5f), 0.3f, Color.BLUE));
         //scene01.addObject(new Sphere(new Vector3D(4.85f, 1f, 4.5f), 0.3f, Color.PINK));
         //scene01.addObject(new Sphere(new Vector3D(2.85f, 1f, 304.5f), 0.5f, Color.BLUE, teaPot));
         //scene01.addObject(OBJReader.GetPolygon("Cube.obj", new Vector3D(0f, -2.5f, 1f), Color.WHITE));
         //scene01.addObject(OBJReader.GetPolygon("CubeQuad.obj", new Vector3D(-3f, -2.5f, 3f), Color.GREEN));
-        scene01.addObject(OBJReader.GetPolygon("SmallTeapot.obj", new Vector3D(-1f, -3f, 1f), Color.BLUE, teaPot));
+        //scene01.addObject(OBJReader.GetPolygon("skullrotated.obj", new Vector3D(0f, -5.5f, 1f), Color.DARK_GRAY, teaPot));
+        scene01.addObject(OBJReader.GetPolygon("SizedJoker3.obj", new Vector3D(0f, -3f, 4f), new Color(124, 0, 199), teaPot));
+        scene01.addObject(OBJReader.GetPolygon("BatarangSized.obj", new Vector3D(0f, -2.5f, 1f), Color.DARK_GRAY, Batarang));
         //scene01.addObject(OBJReader.GetPolygon("Ring.obj", new Vector3D(2f, -1.0f, 1.5f), Color.BLUE));
-        scene01.addObject(OBJReader.GetPolygon("yamelosaque.obj", new Vector3D(0f, -6.1f, 0f), Color.red, material2));
+        //scene01.addObject(OBJReader.GetPolygon("BigWolfRotated.obj", new Vector3D(1,-6.5,2), new Color(52, 52, 52), Wolf));
+        //scene01.addObject((OBJReader.GetPolygon("candle1.obj", new Vector3D(.3f, -7f, 5f), new Color(255, 242, 202), Candle)));
+        //scene01.addObject((OBJReader.GetPolygon("candle1.obj", new Vector3D(5.5f, -7f, 5f), new Color(255, 242, 202), Candle)));
+        scene01.addObject(OBJReader.GetPolygon("yamelosaque.obj", new Vector3D(0f, -6f, 0f), Color.DARK_GRAY, plane));
+        scene01.addObject(OBJReader.GetPolygon("yamelosaque.obj", new Vector3D(-6.3f, -6f, 0f), Color.DARK_GRAY, plane));
+        scene01.addObject(OBJReader.GetPolygon("yamelosaque.obj", new Vector3D(6.3f, -6f, 0f), Color.DARK_GRAY, plane));
+        scene01.addObject(OBJReader.GetPolygon("yamelosaque.obj", new Vector3D(0f, -6f, 6.3f), Color.DARK_GRAY, plane));
+        scene01.addObject(OBJReader.GetPolygon("yamelosaque.obj", new Vector3D(-6.3f, -6f, 6.3f), Color.DARK_GRAY, plane));
+        scene01.addObject(OBJReader.GetPolygon("yamelosaque.obj", new Vector3D(6.3f, -6f, 6.3f), Color.DARK_GRAY, plane));
+
+
 
 
         BufferedImage image = raytrace(scene01);
@@ -85,13 +110,24 @@ public class Raytracer {
                     for (Light light : lights)
                     {
                         pixelColor = addColor(pixelColor, getObjectColor(closestIntersection,light,objects,pixelColor,mainCamera, nearFarPlanes));
+
                         if(closestIntersection.getObject().getMaterial().getReflection() != 0)
                         {
 
                             //pixelColor = addColor(pixelColor, specular(light, closestIntersection, closestIntersection.getObject(), nearFarPlanes));
+
+                            //adding reflection
                             pixelColor = addColor(pixelColor, getObjectColor(closestIntersection,light, objects, pixelColor, mainCamera, nearFarPlanes));
                             Color reflectiveIntersection = reflect(closestIntersection, mainCamera, objects, nearFarPlanes, light, cameraZ);
                             pixelColor = addColor(pixelColor, reflectiveIntersection);
+
+                        }
+
+                        if(closestIntersection.getObject().getMaterial().getRefraction() != 0)
+                        {
+                            pixelColor = addColor(pixelColor, getObjectColor(closestIntersection,light, objects, pixelColor, mainCamera, nearFarPlanes));
+                            Color refractiveIntersection = refract(closestIntersection, mainCamera, objects, nearFarPlanes, light, cameraZ);
+                            pixelColor = addColor(pixelColor, refractiveIntersection);
 
                         }
 
@@ -176,12 +212,52 @@ public class Raytracer {
         {
 
             reflectColor = getObjectColor(reflectIntersection, light, objects, reflectColor, mainCamera, nearFarPlanes);
+            //float reflectIndex = closestIntersection.getObject().getMaterial().getReflection();
+            //float[] rgbColor = new float[]{reflectColor.getRed() * reflectIndex, reflectColor.getGreen() * reflectIndex, reflectColor.getBlue() * reflectIndex};
+            //reflectColor = new Color(clamp(rgbColor[0], 0, .3f), clamp(rgbColor[1],0,.3f), clamp(rgbColor[2],0,.3f));
             //reflectColor = addColor(reflectColor, reflect(closestIntersection, mainCamera, objects, nearFarPlanes, light, cameraZ));
-
 
         }
 
         return reflectColor;
+    }
+
+    public static Color refract(Intersection closestIntersection, Camera mainCamera, ArrayList<Object3D> objects, float[] nearFarPlanes, Light light, float cameraZ)
+    {
+        double n1 = 1;
+        double n2 = 1.5;
+
+        Vector3D incidentRay = Vector3D.substract(closestIntersection.getPosition(), mainCamera.getPosition());
+        double c1 = Vector3D.dotProduct(closestIntersection.getNormal(), incidentRay);
+        double n =  n1 / n2;
+
+        double c2 = Math.sqrt(1-Math.pow(n, 2) * (1 - Math.pow(c1, 2)));
+
+        Vector3D n_I = Vector3D.scalarMultiplication(incidentRay, n);
+        double n_c1MinusC2 = (n * c1) - c2;
+        Vector3D n_c1MinusC2dotN = Vector3D.scalarMultiplication(closestIntersection.getNormal(),n_c1MinusC2);
+        Vector3D T = Vector3D.add(n_I, n_c1MinusC2dotN);
+
+
+        Color refractColor = Color.BLACK;
+
+        Ray reflectHit = new Ray(closestIntersection.getPosition(), T);
+        nearFarPlanes = new float[]{cameraZ + nearFarPlanes[0], cameraZ + nearFarPlanes[1]};
+        Intersection reflectIntersection = raycast(reflectHit,objects,closestIntersection.getObject(),nearFarPlanes);
+
+        if(reflectIntersection != null && closestIntersection.getObject() != reflectIntersection.getObject())
+        {
+
+            refractColor = getObjectColor(reflectIntersection, light, objects, refractColor, mainCamera, nearFarPlanes);
+            //float reflectIndex = closestIntersection.getObject().getMaterial().getReflection();
+            //float[] rgbColor = new float[]{reflectColor.getRed() * reflectIndex, reflectColor.getGreen() * reflectIndex, reflectColor.getBlue() * reflectIndex};
+            //reflectColor = new Color(clamp(rgbColor[0], 0, .3f), clamp(rgbColor[1],0,.3f), clamp(rgbColor[2],0,.3f));
+            //reflectColor = addColor(reflectColor, reflect(closestIntersection, mainCamera, objects, nearFarPlanes, light, cameraZ));
+
+        }
+
+        return refractColor;
+
     }
 
     public static Color getObjectColor(Intersection closestIntersection, Light light, ArrayList<Object3D> objects, Color pixelColor, Camera mainCamera, float[] nearFarPlanes)
